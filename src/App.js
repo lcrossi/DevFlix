a670e93122732d7de8e16797c3ed753e9416e9c6
Luis Rossi <lcrossi2010@gmail.com>
2020-07-27 23:35:05 -0300

    Adicionando footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Menu from './components/Menu';
 //import Caroussel from './components/Carousel';
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div style={{background: "#141414"}}>
       <Menu/>
-
+      <Footer/>
     </div>
   );
 }
